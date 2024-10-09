2b27333cefcdbf80cb7d6bca754c1bb1723df8c8
martonlori <marton.lori09@gmail.com>
2024-10-09 15:57:09 +0200

    Ongoing implementation of register route, bugfixing

--- my_flask_app.py
+++ my_flask_app.py
@@ -50,3 +50,15 @@ def login():
 
 @app.route("/register", methods=["POST"])
 def register():
+    db, connection = get_db_connection()
+    #commit on connection
+    #close on connection
+    hashed_password_bytestring = hash_password() #Get user password, hash it with adding salt, return it as hashed_password
+    hashed_password = hashed_password_bytestring.decode('utf-8')
+    username = request.form.get("username") #Get username from form
+    print(username, hashed_password)
+    db.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+    connection.commit()
+    connection.close()
+    return render_template ("/")
+
