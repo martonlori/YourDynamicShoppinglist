14096f34b1d36c491cdb81ccf419c04572ec97e4
martonlori <marton.lori09@gmail.com>
2024-10-13 15:14:12 +0200

    Changed signup route to register - post and get method into 1 route, merged the signup and register routes and html under register name for future reference, added error checking for username entering on register route

--- my_flask_app.py
+++ my_flask_app.py
@@ -46,36 +46,39 @@ def shoppinglist():
 def shoppingmate():
     return render_template("shoppingmate.html")
 
-@app.route("/signup",methods=["GET"])
-def signup():
-    return render_template("signup.html")
 
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
 
-@app.route("/register", methods=["POST"])
+@app.route("/register", methods=["GET","POST"])
 def register():
-    username = request.form.get("username") #Get username from form
-    password = request.form.get("password") #Get password from form
-    password_confirmation = request.form.get("passwordcheck") #Get the password confirmation
-    db, connection = get_users_db_connection()
-
-    if db.execute("SELECT * FROM users WHERE username = ?", [username]).fetchone() is None and password == password_confirmation:
-        hashed_password_bytestring = hash_password() #Get user password, hash it with adding salt, return it as hashed_password
-        hashed_password = hashed_password_bytestring.decode('utf-8')
-        db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashed_password])
-        connection.commit()
-        connection.close()
-        flash('Registered successfully!', 'success')
-        return redirect (url_for('login'))
+    if request.method == "GET":
+        return render_template("register.html")
     
-    elif db.execute("SELECT * FROM users WHERE username = ?", [username]).fetchone() is not None:
-        flash('Username already exists, please try registering again, with a different username.', 'danger')
-        return render_template("signup.html")
+    else:
+        username = request.form.get("username") #Get username from form
+        password = request.form.get("password") #Get password from form
+        password_confirmation = request.form.get("passwordcheck") #Get the password confirmation
+        db, connection = get_users_db_connection()
 
-    elif password != password_confirmation:
-        flash('The passwords did not match, please ensure, that you are entering the same password twice.', 'danger')
-        return render_template("signup.html")
-        #Add error message
+        if db.execute("SELECT * FROM users WHERE username = ?", [username]).fetchone() is None and password == password_confirmation and username:
+            hashed_password_bytestring = hash_password() #Get user password, hash it with adding salt, return it as hashed_password
+            hashed_password = hashed_password_bytestring.decode('utf-8')
+            db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashed_password])
+            connection.commit()
+            connection.close()
+            flash('Registered successfully!', 'success')
+            return redirect (url_for('login'))
+        
+        elif db.execute("SELECT * FROM users WHERE username = ?", [username]).fetchone() is not None:
+            flash('Username already exists, please try registering again, with a different username.', 'danger')
+            return render_template("register.html")
 
+        elif password != password_confirmation:
+            flash('The passwords did not match, please ensure, that you are entering the same password twice.', 'danger')
+            return render_template("register.html")
+        
+        elif not username:
+            flash('Please enter a username to register!', 'danger')
+            return render_template("register.html")
